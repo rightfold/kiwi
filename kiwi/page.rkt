7b8ebb67b61d71870258239b03e6e40e9e99210d
rightfold <rightfold@gmail.com>
2016-05-15 18:46:08 +0200

    Add nice style sheet

--- kiwi/page.rkt
+++ kiwi/page.rkt
@@ -3,6 +3,5 @@
 (define (page-path page)
   (let* ((base (current-directory))
          (full (build-path base page)))
-    (print full (current-error-port))
     ; BUG: check whether full is a subpath of base
     full))
